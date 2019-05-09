2959533a41f98897c2c4ff01094dcfe9f62953fc
conatuseus <conatuseus@gmail.com>
2019-05-09 15:50:28 +0900

    Add toString method

--- src/main/java/racingcar/RacingCar.java
+++ src/main/java/racingcar/RacingCar.java
@@ -1,11 +1,18 @@
 package racingcar;
 
 import racingcar.controller.Controller;
+import racingcar.model.CarList;
 import racingcar.view.View;
 
 public class RacingCar {
     public static void main(String[] args) {
         Controller controller = new Controller();
-        controller.run(View.inputNameOfCar(), View.inputNumberOfTry());
+        CarList carList = controller.makeCarList(View.inputNameOfCar());
+
+        int numberOfTry = View.inputNumberOfTry();
+        for (int i = 0; i < numberOfTry; i++) {
+            carList = controller.moveCar(carList);
+
+        }
     }
 }
--- src/main/java/racingcar/model/CarList.java
+++ src/main/java/racingcar/model/CarList.java
@@ -5,22 +5,14 @@ import java.util.List;
 
 public class CarList {
     private List<Car> carList = new ArrayList<>();
-    private final int numberOfTry;
 
-    public CarList(String[] carNames, int numberOfTry) {
+    public CarList(String[] carNames) {
         for (String carName : carNames) {
             carList.add(new Car(carName));
         }
-        this.numberOfTry=numberOfTry;
     }
 
-    public void moveCarsByNumberOfTry() {
-        for (int i = 0; i < this.numberOfTry; i++) {
-            moveCars();
-        }
-    }
-
-    private void moveCars() {
+    public void moveCars() {
         for (Car car : carList) {
             car.move(Random.isEnableGo());
         }
