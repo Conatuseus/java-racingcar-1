d88efcc99e66d395af09b13d95e3f2ef583fafd8
conatuseus <conatuseus@gmail.com>
2019-05-13 17:42:57 +0900

    WinningCars 코드 추가 및 Test Code 변경

--- src/main/java/racingcar/model/WinningCars.java
+++ src/main/java/racingcar/model/WinningCars.java
@@ -28,6 +28,11 @@ public class WinningCars {
         }
     }
 
+    /* Test를 위한 Code */
+    public boolean isWinner(String name) {
+        return winningCarNames.contains(name);
+    }
+
     @Override
     public String toString() {
         return String.join(", ", winningCarNames) + " 가 최종 우승했습니다.";
